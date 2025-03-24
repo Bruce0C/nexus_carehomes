5bcd0c89af9e5949814c6a4c5540ab65b314dcbb
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2025-03-24 23:50:15 +0000

    Add variables for sheets

--- run.py
+++ run.py
@@ -12,3 +12,8 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('nexuscare')
+
+user = SHEET.worksheet('user')
+home = SHEET.worksheet('home')
+medication = SHEET.worksheet('medication')
+schedule = SHEET.worksheet('schedule')
