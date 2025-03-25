ffdb9bbe5a56bae294bf6e29a14e283be1fb0757
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2025-03-25 00:41:18 +0000

    Remove unused variables

--- run.py
+++ run.py
@@ -8,12 +8,12 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
+
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('nexuscare')
 
 user = SHEET.worksheet('user')
-home = SHEET.worksheet('home')
-medication = SHEET.worksheet('medication')
-schedule = SHEET.worksheet('schedule')
+
+print(user)
